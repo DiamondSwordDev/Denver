4566da132e38626316c6688218cc0a408e3d3c5e
Nathan2055 <spychicken2055+code@gmail.com>
2015-12-30 14:48:31 -0500

    Set look and feel to system default

--- Denver/src/com/dsdev/colorado/denver/MainFrame.java
+++ Denver/src/com/dsdev/colorado/denver/MainFrame.java
@@ -1,8 +1,12 @@
 package com.dsdev.colorado.denver;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
+ * This is the main code for the splash screen with the loading bar.
  *
- * @author Greenlock
+ * @author Nathan Larsen
  */
 public class MainFrame extends javax.swing.JFrame {
 
@@ -42,28 +46,17 @@ public class MainFrame extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+        // Set L&F to system default instead of the cross-platform Java thingy
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, e);
         }
-        //</editor-fold>
+        // handle exception
+        // handle exception
+        // handle exception
+
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
